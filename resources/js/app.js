31b1510a07eb493d1f69f917bd839cf0a1c9c651
Emir Kıvrak <emirccus@gmail.com>
2020-04-17 20:02:33 +0300

    router added

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,22 +4,27 @@
  * building robust, powerful web applications using Vue and Laravel.
  */
 
+
 require('./bootstrap');
 
+// vue ve vue routeri dahil ettik
 window.Vue = require('vue');
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
 
-/**
- * The following block of code may be used to automatically register your
- * Vue components. It will recursively scan this directory for the Vue
- * components and automatically register them with their "basename".
- *
- * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
- */
 
-// const files = require.context('./', true, /\.vue$/i)
-// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+
+// hangi rotada hangi component kullanılacağını belirttik
+let routes = [
+    { path: '/foo', component: require('./components/foo.vue').default },
+    { path: '/bar', component: require('./components/bar.vue').default }
+]
+
+//// route dizisindeki rotaları kullan
+const router = new VueRouter({
+    routes
+})
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -29,4 +34,5 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
 
 const app = new Vue({
     el: '#app',
+    router
 });
