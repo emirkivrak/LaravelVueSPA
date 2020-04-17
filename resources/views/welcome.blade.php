328aae3bdbf8e1e58deb9ed5641e7e4ce00cefdf
Emir Kıvrak <emirccus@gmail.com>
2020-04-17 17:23:47 +0300

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        <!-- Styles -->
+    </head>
+    <body>
+        <div class="md:flex container border p-4">
+            <div class="md:flex-shrink-0">
+                <img class="rounded-lg md:w-56" src="https://images.unsplash.com/photo-1556740738-b6a63e27c4df?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=448&q=80" alt="Woman paying for a purchase">
+            </div>
+            <div class="mt-4 md:mt-0 md:ml-6">
+                <div class="uppercase tracking-wide text-sm text-indigo-600 font-bold">HAVE A NICE DAY</div>
+                <a href="#" class="block mt-1 text-lg leading-tight font-semibold text-gray-900 hover:underline">İf you see this page laravel successfully installed and serving</a>
+                <p class="mt-2 text-gray-600">if this page looks good TAILWIND MOUNTED! </p>
+                <p class="mt-2 text-gray-600">If not :( check steps again </p>
+            </div>
+        </div>
+    </body>
+</html>
